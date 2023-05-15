880686de8ae6d56ea468a2c9ea6e97b374cc9d19
Luiz Felipe <luizfelipe0527@gmail.com>
2023-05-14 23:39:24 -0300

    Appointment router implemented

--- backend/src/routes/Router.ts
+++ backend/src/routes/Router.ts
@@ -2,13 +2,14 @@ import express from "express";
 
 // Routes
 import { UserRoutes } from "./UserRoutes";
+import { AppointmentRoutes } from "./AppointmentRoutes";
 
 class Router {
     private router = express();
 
     execute() {
         this.router.use("/api/users", new UserRoutes().routes());
-        // this.router.use("/api/appointments");
+        this.router.use("/api/appointments", new AppointmentRoutes().routes());
 
         return this.router;
     }
