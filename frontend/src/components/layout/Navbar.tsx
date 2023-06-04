0642a5e7ab1e3950198c45c3ceb27296f3600e76
Luiz Felipe <luizfelipe0527@gmail.com>
2023-06-04 18:06:46 -0300

    improvement: Adjust auth elements

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -2,16 +2,32 @@
 import styles from "./Navbar.module.css";
 
 // Icons
-import { BsFillCalendarWeekFill, BsFillPersonFill, BsBoxArrowInRight, BsPersonAdd } from "react-icons/bs";
+import { 
+    BsFillCalendarWeekFill, 
+    BsFillPersonFill, 
+    BsBoxArrowInRight, 
+    BsPersonAdd, 
+    BsBoxArrowLeft 
+} from "react-icons/bs";
 
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useHandleUser } from "../../hooks/useHandleUser";
+import { useAuth } from "../../hooks/useAuth";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    
+    const { signOut } = useHandleUser();
+    const { authUser } = useAuth();
 
     const toggleDropdownMenu = (): void => setIsOpen((isOpen) => !isOpen);
     const closeDropdownMenu = (): void => setIsOpen(false);
+
+    const logout = (): void => {
+        signOut();
+        closeDropdownMenu();
+    }
 
     return (
         <nav className={styles.navbar}>
@@ -21,19 +37,34 @@ const Navbar = () => {
             </div>
             <div className={styles.dropdown}>
                 <div className={`${styles.user_container} ${isOpen ? styles.active_menu : ""}`}>
-                    <div className={styles.user} onClick={toggleDropdownMenu}>
-                        <BsFillPersonFill />
-                    </div>
+                    {!authUser ? (
+                        <div className={styles.user} onClick={toggleDropdownMenu}>
+                            <BsFillPersonFill />
+                        </div>
+                    ) : (
+                        <div className={`${styles.user} ${styles.signed_user}`} onClick={toggleDropdownMenu}>
+                            {authUser.name.split("")[0].toUpperCase()}
+                        </div>
+                    )}
                 </div>
                 <div className={`${styles.dropdown_menu} ${isOpen ? styles.open : ""}`}>
-                    <Link to="/login" onClick={closeDropdownMenu}>
-                        <BsBoxArrowInRight />
-                        <span>Entrar</span>
-                    </Link>
-                    <Link to="/register" onClick={closeDropdownMenu}>
-                        <BsPersonAdd />
-                        <span>Cadastrar</span>
-                    </Link>
+                {!authUser ? (
+                    <>
+                        <Link to="/login" onClick={closeDropdownMenu}>
+                            <BsBoxArrowInRight />
+                            <span>Entrar</span>
+                        </Link>
+                        <Link to="/register" onClick={closeDropdownMenu}>
+                            <BsPersonAdd />
+                            <span>Cadastrar</span>
+                        </Link>
+                    </>
+                ) : (
+                    <button onClick={logout}>
+                        <BsBoxArrowLeft />
+                        <span>Sair</span>
+                    </button>
+                )}
                 </div>
             </div>
         </nav>
