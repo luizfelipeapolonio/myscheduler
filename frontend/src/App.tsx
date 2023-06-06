5fec5d1be3eaeaaf25339fc399d4d17ba8137c0f
Luiz Felipe <luizfelipe0527@gmail.com>
2023-06-06 17:40:02 -0300

    feat: implement edit profile route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import styles from "./App.module.css";
 import Calendar from "./pages/Calendar";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import EditProfile from "./pages/EditProfile/EditProfile";
 
 // Components
 import Root from "./components/layout/Root";
@@ -26,6 +27,7 @@ function App() {
     createRoutesFromElements(
       <Route path="/" element={loading ? <Loading type="full" /> : <Root />}>
         <Route index path="/" element={authUser ? <Calendar /> : <Navigate to="/login" />} />
+        <Route path="/user" element={authUser ? <EditProfile /> : <Navigate to="/login" />} />
         <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/" />} />
         <Route path="/register" element={!authUser ? <Register /> : <Navigate to="/" />} />
       </Route>
