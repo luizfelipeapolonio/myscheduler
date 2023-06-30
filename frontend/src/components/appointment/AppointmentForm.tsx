bbb3c155bff6a54f7b74eaa654473a78b0ba549b
Luiz Felipe <luizfelipe0527@gmail.com>
2023-06-30 20:44:29 -0300

    improvement: adjust layout

--- frontend/src/components/appointment/AppointmentForm.tsx
+++ frontend/src/components/appointment/AppointmentForm.tsx
@@ -11,53 +11,57 @@ const AppointmentForm = () => {
                 <fieldset className={styles.appointment_type}>
                     <legend>Tipo</legend>
                     <label>
-                        <input name="lembrete" type="radio" value="lembrete" />
+                        <input name="type" type="radio" value="lembrete" />
                         <span>Lembrete</span>
                     </label>
                     <label>
-                        <input name="tarefa" type="radio" value="tarefa" />
+                        <input name="type" type="radio" value="tarefa" />
                         <span>Tarefa</span>
                     </label>
                     <label>
-                        <input name="evento" type="radio" value="evento" />
+                        <input name="type" type="radio" value="evento" />
                         <span>Evento</span>
                     </label>
                 </fieldset>
                 <fieldset className={styles.appointment_priority}>
                     <legend>Prioridade</legend>
                     <label>
-                        <input name="alta" type="radio" value="alta" />
+                        <input name="priority" type="radio" value="alta" />
                         <span>Alta</span>
                     </label>
                     <label>
-                        <input name="media" type="radio" value="media" />
+                        <input name="priority" type="radio" value="media" />
                         <span>Média</span>
                     </label>
                     <label>
-                        <input name="baixa" type="radio" value="baixa" />
+                        <input name="priority" type="radio" value="baixa" />
                         <span>Baixa</span>
                     </label>
                 </fieldset>
                 <fieldset className={styles.appointment_date}>
                     <legend>Data</legend>
-                    <span>29 de Junho de 2023</span>
-                    <label>
-                        <input name="all-day" type="radio" value="default" />
-                        <span>O dia inteiro</span>
-                    </label>
-                    <label>
-                        <input name="add-hour" type="radio" value="hour" />
-                        <span>Adicionar horário</span>
-                    </label>
-                    <select>
-                        <option value="default">Hora</option>
-                    </select>
-                    <select>
-                        <option value="default">Minuto</option>
-                    </select>
+                    <p>29 de Junho de 2023</p>
+                        <label>
+                            <input name="date" type="radio" value="default" />
+                            <span>O dia inteiro</span>
+                        </label>
+                        <label>
+                            <input name="date" type="radio" value="hour" />
+                            <span>Adicionar horário</span>
+                        </label>
+                    <div className={styles.hour}>
+                        <select>
+                            <option value="default">Hora</option>
+                            <option>12</option>
+                            <option>13</option>
+                        </select>
+                        <select>
+                            <option value="default">Minuto</option>
+                        </select>
+                    </div>
                 </fieldset>
                 <textarea 
-                    rows={8} 
+                    rows={4} 
                     placeholder="Adicione uma descrição para o seu compromisso..." 
                 />
                 <input type="submit" value="Agendar compromisso" />
