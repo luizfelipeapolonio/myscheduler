2a74a035952178246a0b18792c916646f55f1cb5
Luiz Felipe <luizfelipe0527@gmail.com>
2023-07-05 20:01:10 -0300

    feat: implement date to schedule context provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import './index.css'
 
 import { AuthContextProvider } from "./context/Auth/AuthContextProvider.tsx";
+import { DateToScheduleProvider } from "./context/Date/DateToScheduleProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <DateToScheduleProvider>
+        <App />
+      </DateToScheduleProvider>
     </AuthContextProvider>
   </React.StrictMode>,
 )
