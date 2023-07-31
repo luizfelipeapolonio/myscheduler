0dd52da00f4ea76f797ec342f10d7f9478e6e681
Luiz Felipe <luizfelipe0527@gmail.com>
2023-07-31 19:55:53 -0300

    feat: add delete status

--- frontend/src/context/Appointment/AppointmentStatus.ts
+++ frontend/src/context/Appointment/AppointmentStatus.ts
@@ -3,8 +3,10 @@ import { createContext, useContext } from "react";
 interface IAppointmentStatus {
     created: boolean;
     edited: boolean;
+    deleted: boolean;
     setCreated: React.Dispatch<React.SetStateAction<boolean>>;
     setEdited: React.Dispatch<React.SetStateAction<boolean>>;
+    setDeleted: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const AppointmentStatusContext = createContext<IAppointmentStatus>(null!);
--- frontend/src/context/Appointment/AppointmentStatusProvider.tsx
+++ frontend/src/context/Appointment/AppointmentStatusProvider.tsx
@@ -9,10 +9,11 @@ interface ChildrenProp {
 export const AppointmentStatusProvider = ({ children }: ChildrenProp) => {
     const [created, setCreated] = useState<boolean>(false);
     const [edited, setEdited] = useState<boolean>(false);
+    const [deleted, setDeleted] = useState<boolean>(false);
 
     return (
         <AppointmentStatusContext.Provider 
-            value={{ created, edited, setCreated, setEdited }}
+            value={{ created, edited, deleted, setCreated, setEdited, setDeleted }}
         >
             {children}
         </AppointmentStatusContext.Provider>
